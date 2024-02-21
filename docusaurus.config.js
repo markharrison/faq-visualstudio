a1f23b16a3a026f046891e340190208521904271
markharrison <mark@harrison.ws>
2024-02-21 22:33:38 +0000

    Visual Studio on EA Benefits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
             label: 'Visual Studio',
           },
           {
-            href: 'https://github.com/markharrison/visualstudio',
+            href: 'https://github.com/markharrison/faq-visualstudio',
             label: 'GitHub',
             position: 'right'
           },
